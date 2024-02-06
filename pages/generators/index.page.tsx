892de14ea7bcaff43648b3f6475049332e3507ff
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-06 18:32:10 +0900

    feat: added new pages for contrast, generators, history, palettes

---
+++ pages/generators/index.page.tsx
@@ -0,0 +1,21 @@
+import type { InferGetServerSidePropsType, NextPage } from "next"
+import { useI18n } from "contexts/i18n-context"
+import { AppLayout } from "layouts/app-layout"
+import { getServerSideCommonProps } from "utils/next"
+
+type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const Page: NextPage<PageProps> = ({}) => {
+  const { t } = useI18n()
+
+  return (
+    <AppLayout
+      title={t("generators.title")}
+      description={t("generators.description")}
+    ></AppLayout>
+  )
+}
+
+export default Page
+
+export const getServerSideProps = getServerSideCommonProps

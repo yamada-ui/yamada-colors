847483fdbfe59abb46563f7b3716b023ddaf7af0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-06 13:04:28 +0900

    feat(footer): added social links and description

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -1,11 +1,23 @@
 import type { CenterProps } from "@yamada-ui/react"
-import { Center, forwardRef } from "@yamada-ui/react"
+import {
+  Center,
+  HStack,
+  Link,
+  Text,
+  VStack,
+  forwardRef,
+} from "@yamada-ui/react"
 import { memo } from "react"
+import { Github, Twitter } from "components/media-and-icons"
+import { CONSTANT } from "constant"
+import { useI18n } from "contexts/i18n-context"
 
 export type FooterProps = CenterProps & {}
 
 export const Footer = memo(
   forwardRef<FooterProps, "div">(({ ...rest }, ref) => {
+    const { tc } = useI18n()
+
     return (
       <Center
         ref={ref}
@@ -14,7 +26,54 @@ export const Footer = memo(
         top="100vh"
         w="full"
         {...rest}
-      ></Center>
+      >
+        <VStack
+          alignItems="center"
+          w="full"
+          maxW="9xl"
+          py="xl"
+          px={{ base: "lg", md: "md" }}
+        >
+          <Text
+            color={["blackAlpha.600", "whiteAlpha.600"]}
+            fontSize={{ base: "md", sm: "sm" }}
+            textAlign="center"
+          >
+            {tc("component.footer.description", (str, index) => (
+              <Link
+                href={
+                  index === 1
+                    ? CONSTANT.SNS.GITHUB.HIROTOMO_YAMADA
+                    : CONSTANT.SNS.GITHUB.YAMADA_COLORS + "/graphs/contributors"
+                }
+                target="_blank"
+              >
+                {str}
+              </Link>
+            ))}
+          </Text>
+
+          <HStack>
+            <Link
+              color="muted"
+              href={CONSTANT.SNS.GITHUB.HIROTOMO_YAMADA}
+              target="_blank"
+              aria-label="GitHub profile of Hirotomo Yamada"
+            >
+              <Github />
+            </Link>
+
+            <Link
+              color="muted"
+              href={CONSTANT.SNS.TWITTER.HIROTOMO_YAMADA}
+              target="_blank"
+              aria-label="Twitter Hirotomo Yamada"
+            >
+              <Twitter />
+            </Link>
+          </HStack>
+        </VStack>
+      </Center>
     )
   }),
 )

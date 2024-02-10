8a3a31697208c88f8643e98d35588123f1638e45
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-10 15:42:24 +0900

    feat(next.config): added api file extensions

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   optimizeFonts: true,
   reactStrictMode: false,
-  pageExtensions: ["page.jsx", "page.tsx"],
+  pageExtensions: ["page.jsx", "page.tsx", "api.js", "api.ts"],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja"],

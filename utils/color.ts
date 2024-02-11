a41f30470de3a2c482c5dce321d5c6125066ea64
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-11 18:16:42 +0900

    refactor(utils): simplified color conversion functions

--- utils/color.ts
+++ utils/color.ts
@@ -3,11 +3,11 @@ import * as color from "color2k"
 
 export const isLight = (hex: string) => color.readableColorIsBlack(hex)
 
-export const toRgba = (hex: string) => convert.hex.rgb(hex)
+export const toRgb = (hex: string) => convert.hex.rgb(hex)
 
-export const toHsla = (hex: string) => convert.hex.hsl(hex)
+export const toHsl = (hex: string) => convert.hex.hsl(hex)
 
-export const toHsva = (hex: string) => convert.hex.hsv(hex)
+export const toHsv = (hex: string) => convert.hex.hsv(hex)
 
 export const toCmyk = (hex: string) => convert.hex.cmyk(hex)
 

62afbb71c649ad2fa2711667b613d01142ba88f3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-11 17:33:49 +0900

    refactor(utils): updated color conversion methods

--- utils/color.ts
+++ utils/color.ts
@@ -1,3 +1,16 @@
-import * as c from "color2k"
+import convert from "color-convert"
+import * as color from "color2k"
 
-export const isLight = (color: string) => c.readableColorIsBlack(color)
+export const isLight = (hex: string) => color.readableColorIsBlack(hex)
+
+export const toRgba = (hex: string) => convert.hex.rgb(hex)
+
+export const toHsla = (hex: string) => convert.hex.hsl(hex)
+
+export const toHsva = (hex: string) => convert.hex.hsv(hex)
+
+export const toCmyk = (hex: string) => convert.hex.cmyk(hex)
+
+export const toCielab = (hex: string) => convert.hex.lab(hex)
+
+export const toCielch = (hex: string) => convert.hex.lch(hex)

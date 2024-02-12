ebd8291673ec3255d85a3e20983400afd3b1ffc8
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-12 13:37:21 +0900

    feat: added gradient component to color pages

---
+++ pages/colors/[hex]/gradient.tsx
@@ -0,0 +1,112 @@
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Motion,
+  Text,
+  VStack,
+} from "@yamada-ui/react"
+import Link from "next/link"
+import type { FC, ReactNode } from "react"
+import { NextLink } from "components/navigation"
+import { useI18n } from "contexts/i18n-context"
+
+export type GradientProps = {
+  hex: string
+  shadeColors: Colors
+  tintColors: Colors
+}
+
+export const Gradient: FC<GradientProps> = ({
+  hex,
+  shadeColors,
+  tintColors,
+}) => {
+  const { t } = useI18n()
+
+  return (
+    <Grid templateColumns={{ base: "repeat(3, 1fr)" }}>
+      <List
+        title={t("colors.shade.title")}
+        description={t("colors.shade.description", hex)}
+        more={t("colors.shade.more")}
+        href={`/generators?hex=${hex.replace("#", "")}&tab=shade`}
+        colors={shadeColors}
+      />
+      <List
+        title={t("colors.tint.title")}
+        description={t("colors.tint.description", hex)}
+        more={t("colors.tint.more")}
+        href={`/generators?hex=${hex.replace("#", "")}&tab=tint`}
+        colors={tintColors}
+      />
+      <List
+        title={t("colors.gray.title")}
+        description={t("colors.gray.description", hex)}
+        more={t("colors.gray.more")}
+        href={`/generators?hex=${hex.replace("#", "")}&tab=gray`}
+        colors={[]}
+      />
+    </Grid>
+  )
+}
+
+type ListProps = {
+  title: ReactNode
+  description: ReactNode
+  more?: ReactNode
+  href?: string
+  colors: Colors
+}
+
+const List: FC<ListProps> = ({ title, description, more, href, colors }) => {
+  return (
+    <GridItem as="section" display="flex" flexDirection="column" gap="md">
+      <VStack gap={{ base: "xs", sm: "0" }}>
+        <Heading fontSize={{ base: "lg" }}>{title}</Heading>
+        <Text color="muted" fontSize="sm">
+          {description}
+        </Text>
+      </VStack>
+
+      <Box as="nav">
+        <VStack as="ul">
+          {colors.slice(1, 5).map(({ name, hex }, index) => (
+            <Motion
+              key={`${hex}-${index}`}
+              as="li"
+              rounded="2xl"
+              whileHover={{ scale: 0.95 }}
+            >
+              <Grid
+                as={Link}
+                href={`/colors/${hex.replace("#", "")}`}
+                templateColumns={{ base: "auto 1fr" }}
+                gap={{ base: "md" }}
+              >
+                <Box boxSize={{ base: "12" }} bg={hex} rounded="2xl" />
+
+                <VStack gap={{ base: "xs", sm: "0" }} justifyContent="center">
+                  <Text as="span" fontWeight="medium" lineClamp={1}>
+                    {name}
+                  </Text>
+
+                  <Text as="span" fontSize="sm" color="muted" lineClamp={1}>
+                    {hex}
+                  </Text>
+                </VStack>
+              </Grid>
+            </Motion>
+          ))}
+        </VStack>
+      </Box>
+
+      {more && href ? (
+        <NextLink href={href} variant="muted" fontSize="sm" whiteSpace="nowrap">
+          {more}
+        </NextLink>
+      ) : null}
+    </GridItem>
+  )
+}
--- pages/colors/[hex]/index.page.tsx
+++ pages/colors/[hex]/index.page.tsx
@@ -5,6 +5,7 @@ import type {
   NextPage,
 } from "next"
 import { Data } from "./data"
+import { Gradient } from "./gradient"
 import { Header } from "./header"
 import { useHistory } from "./use-history"
 import { useI18n } from "contexts/i18n-context"
@@ -17,19 +18,67 @@ type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>
 
 export type ColorData = PageProps["data"]
 
-const Page: NextPage<PageProps> = ({ cookies, hex, name, data }) => {
+const Page: NextPage<PageProps> = ({
+  cookies,
+  hex,
+  name,
+  data,
+  shadeColors,
+  tintColors,
+}) => {
   useHistory({ cookies, hex })
   const { t } = useI18n()
 
   return (
-    <AppLayout title={hex} description={t("app.description")} gap="lg">
+    <AppLayout title={hex} description={t("colors.description")} gap="lg">
       <Header {...{ hex, name }} />
       <Data {...data} />
+      <Gradient {...{ hex, shadeColors, tintColors }} />
     </AppLayout>
   )
 }
 
 export default Page
+
+const getColorData = (hex: string) => {
+  const name = getColorName(hex)
+  const rgb = toRgb(hex)
+  const hsl = toHsl(hex)
+  const hsv = toHsv(hex)
+  const cmyk = toCmyk(hex)
+  const cielab = toCielab(hex)
+  const cielch = toCielch(hex)
+
+  return { name, hex, rgb, hsl, hsv, cmyk, cielab, cielch }
+}
+
+const getShadeColors = (targetHex: string) => {
+  const colors: Colors = []
+
+  for (let i = 0; i < 10; i++) {
+    const amount = (10 - i) / 10
+    const hex = c.toHex(c.darken(targetHex, amount))
+    const name = getColorName(hex)
+
+    colors.push({ hex, name })
+  }
+
+  return colors
+}
+
+const getTintColors = (targetHex: string) => {
+  const colors: Colors = []
+
+  for (let i = 0; i < 10; i++) {
+    const amount = (10 - i) / 10
+    const hex = c.toHex(c.lighten(targetHex, amount))
+    const name = getColorName(hex)
+
+    colors.push({ hex, name })
+  }
+
+  return colors
+}
 
 export const getServerSideProps = async (req: GetServerSidePropsContext) => {
   const {
@@ -40,17 +89,14 @@ export const getServerSideProps = async (req: GetServerSidePropsContext) => {
   try {
     hex = c.toHex(hex)
 
-    const name = getColorName(hex)
-    const rgb = toRgb(hex)
-    const hsl = toHsl(hex)
-    const hsv = toHsv(hex)
-    const cmyk = toCmyk(hex)
-    const cielab = toCielab(hex)
-    const cielch = toCielch(hex)
+    const data = getColorData(hex)
 
-    const data = { name, hex, rgb, hsl, hsv, cmyk, cielab, cielch }
+    const { name } = data
 
-    const props = { cookies, name, hex, data }
+    const shadeColors = getShadeColors(hex)
+    const tintColors = getTintColors(hex)
+
+    const props = { cookies, name, hex, data, shadeColors, tintColors }
 
     return { props }
   } catch {

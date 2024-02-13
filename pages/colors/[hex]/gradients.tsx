42c40fb14d7a8685cddcb90b1748ad7272f43da8
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-14 02:12:13 +0900

    style: added grid gap in gradients component

--- pages/colors/[hex]/gradients.tsx
+++ pages/colors/[hex]/gradients.tsx
@@ -28,7 +28,7 @@ export const Gradients: FC<GradientsProps> = ({
   const { t } = useI18n()
 
   return (
-    <Grid templateColumns={{ base: "repeat(3, 1fr)" }}>
+    <Grid templateColumns={{ base: "repeat(3, 1fr)" }} gap="md">
       <List
         title={t("colors.shades.title")}
         description={t("colors.shades.description", hex)}

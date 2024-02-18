672b1e9822ffcd1c97d2d842e3a662c9528c1060
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-18 17:59:56 +0900

    feat(components): added segmented control component

--- theme/components/index.ts
+++ theme/components/index.ts
@@ -1,5 +1,6 @@
 import { Button } from "./button"
 import { ColorSelector } from "./color-selector"
 import { Link } from "./link"
+import { SegmentedControl } from "./segmented-control"
 
-export const components = { Link, Button, ColorSelector }
+export const components = { Link, Button, ColorSelector, SegmentedControl }

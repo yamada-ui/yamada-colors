29464d61f775c2de3feaca352e25f67922054f34
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-19 00:14:35 +0900

    feat(header): added link to color details

--- pages/generators/header.tsx
+++ pages/generators/header.tsx
@@ -7,6 +7,7 @@ import {
   Text,
   VStack,
 } from "@yamada-ui/react"
+import Link from "next/link"
 import type { FC } from "react"
 
 export type HeaderProps = Color
@@ -14,7 +15,15 @@ export type HeaderProps = Color
 export const Header: FC<HeaderProps> = ({ hex, name }) => {
   return (
     <HStack as="section">
-      <Grid templateColumns={{ base: "auto 1fr" }} gap={{ base: "md" }}>
+      <Grid
+        as={Link}
+        templateColumns={{ base: "auto 1fr" }}
+        gap={{ base: "md" }}
+        href={`/colors/${hex.replace("#", "")}`}
+        outline={0}
+        _focusVisible={{ boxShadow: "outline" }}
+        rounded="2xl"
+      >
         <Box boxSize={{ base: "20", sm: "16" }} bg={hex} rounded="2xl" />
 
         <VStack gap={{ base: "xs", sm: "0" }} justifyContent="center">

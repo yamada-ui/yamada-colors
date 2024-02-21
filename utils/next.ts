6d421bcf5bd6ed838d8e1c350b1f62b53f3495a0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-21 14:22:38 +0900

    feat(utils): added format prop to server-side props

--- utils/next.ts
+++ utils/next.ts
@@ -1,9 +1,11 @@
+import type { ColorFormat } from "@yamada-ui/react"
 import type { GetServerSidePropsContext } from "next"
 
 export const getServerSideCommonProps = async ({
   req,
 }: GetServerSidePropsContext) => {
   const cookies = req.headers.cookie ?? ""
+  const format = (req.headers.format ?? "hex") as ColorFormat
 
-  return { props: { cookies } }
+  return { props: { cookies, format } }
 }

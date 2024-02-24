26bffde11ac9c711525c1032759fd9033e7a2fbd
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-24 21:12:43 +0900

    feat(search-color): added RemoveScroll for color picker

--- components/form/search-color.tsx
+++ components/form/search-color.tsx
@@ -14,6 +14,7 @@ import type {
 } from "@yamada-ui/react"
 import * as c from "color2k"
 import { forwardRef, memo } from "react"
+import { RemoveScroll } from "react-remove-scroll"
 import { useApp } from "contexts/app-context"
 import { useI18n } from "contexts/i18n-context"
 import { f } from "utils/color"
@@ -59,61 +60,63 @@ export const SearchColor = memo(
       }
 
       return (
-        <Box position="relative" h="fit-content" {...containerProps}>
-          <ColorPicker
-            ref={ref}
-            isOpen={isOpen}
-            onOpen={onOpen}
-            onClose={onClose}
-            value={value}
-            onChange={onChange}
-            placeholder={f("#ffffff", format)}
-            matchWidth
-            format={format}
-            rounded="full"
-            eyeDropperProps={{ right: 9, fontSize: "1em", rounded: "full" }}
-            inputProps={{ pe: "4rem" }}
-            onKeyDown={(ev) => {
-              if (ev.key !== "Enter") return
+        <RemoveScroll allowPinchZoom={false} enabled={isOpen} forwardProps>
+          <Box position="relative" h="fit-content" {...containerProps}>
+            <ColorPicker
+              ref={ref}
+              isOpen={isOpen}
+              onOpen={onOpen}
+              onClose={onClose}
+              value={value}
+              onChange={onChange}
+              placeholder={f("#ffffff", format)}
+              matchWidth
+              format={format}
+              rounded="full"
+              eyeDropperProps={{ right: 9, fontSize: "1em", rounded: "full" }}
+              inputProps={{ pe: "4rem" }}
+              onKeyDown={(ev) => {
+                if (ev.key !== "Enter") return
 
-              onSubmit()
-            }}
-            {...rest}
-          >
-            <Button
-              colorScheme="neutral"
-              borderColor="transparent"
-              bg={["blackAlpha.200", "whiteAlpha.100"]}
-              onClick={onSubmit}
+                onSubmit()
+              }}
+              {...rest}
             >
-              {t("component.color-search.submit")}
-            </Button>
-          </ColorPicker>
+              <Button
+                colorScheme="neutral"
+                borderColor="transparent"
+                bg={["blackAlpha.200", "whiteAlpha.100"]}
+                onClick={onSubmit}
+              >
+                {t("component.color-search.submit")}
+              </Button>
+            </ColorPicker>
 
-          <IconButton
-            bg={["blackAlpha.200", "whiteAlpha.100"]}
-            borderColor="transparent"
-            icon={
-              <ChevronIcon
-                fontSize="1.3em"
-                color="muted"
-                transform="rotate(-90deg)"
-              />
-            }
-            colorScheme="neutral"
-            position="absolute"
-            zIndex="kurillin"
-            top="50%"
-            right="2"
-            transform="translateY(-50%)"
-            boxSize="6"
-            minW="auto"
-            size="sm"
-            isRounded
-            {...submitProps}
-            onClick={handlerAll(submitProps?.onClick, onSubmit)}
-          />
-        </Box>
+            <IconButton
+              bg={["blackAlpha.200", "whiteAlpha.100"]}
+              borderColor="transparent"
+              icon={
+                <ChevronIcon
+                  fontSize="1.3em"
+                  color="muted"
+                  transform="rotate(-90deg)"
+                />
+              }
+              colorScheme="neutral"
+              position="absolute"
+              zIndex="kurillin"
+              top="50%"
+              right="2"
+              transform="translateY(-50%)"
+              boxSize="6"
+              minW="auto"
+              size="sm"
+              isRounded
+              {...submitProps}
+              onClick={handlerAll(submitProps?.onClick, onSubmit)}
+            />
+          </Box>
+        </RemoveScroll>
       )
     },
   ),

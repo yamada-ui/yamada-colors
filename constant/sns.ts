80be18f22c942f27e7915a3f5e13dbb8152fd5d1
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-03 17:28:52 +0900

    chore: updated repository links in templates and constants

--- constant/sns.ts
+++ constant/sns.ts
@@ -1,7 +1,7 @@
 export const SNS = {
   GITHUB: {
     HIROTOMO_YAMADA: "https://github.com/hirotomoyamada",
-    YAMADA_COLORS: "https://github.com/hirotomoyamada/yamada-colors",
+    YAMADA_COLORS: "https://github.com/yamada-ui/yamada-colors",
   },
   TWITTER: {
     HIROTOMO_YAMADA: "https://twitter.com/hirotomoyamada",

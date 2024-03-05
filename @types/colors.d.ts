1cdd5f9d7280ab92769ada24e2932caa46b52ed3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-06 00:27:23 +0900

    refactor: updated color types with PaletteColors

--- @types/colors.d.ts
+++ @types/colors.d.ts
@@ -1,13 +1,16 @@
 type Color = { hex: string; name: string }
 type Colors = Color[]
 
-type ReorderColor = Color & { id: string }
+type PaletteColor = { hex: [string, string]; name: string }
+type PaletteColors = PaletteColor[]
+
+type ReorderColor = PaletteColor & { id: string }
 type ReorderColors = ReorderColor[]
 
 type ColorPalette = {
   uuid: string
   name: string
-  colors: Colors
+  colors: PaletteColors
   timestamp: number
 }
 type ColorPalettes = ColorPalette[]

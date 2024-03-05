f9db4da4977fd6e7061c574553054923bf2fc7f3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-06 02:11:53 +0900

    feat(export-modal): added copy to clipboard feature

--- components/overlay/export-modal.tsx
+++ components/overlay/export-modal.tsx
@@ -1,4 +1,6 @@
 import {
+  HStack,
+  IconButton,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -8,12 +10,15 @@ import {
   Tag,
   Text,
   useAsync,
+  useClipboard,
   useUpdateEffect,
 } from "@yamada-ui/react"
 import type { ModalProps, SelectItem } from "@yamada-ui/react"
 import { memo, useState } from "react"
 import type { FC } from "react"
+import { Check, Clipboard } from "components/media-and-icons"
 import { useApp } from "contexts/app-context"
+import { useI18n } from "contexts/i18n-context"
 
 const items: SelectItem[] = [
   {
@@ -54,29 +59,73 @@ export type ExportModalProps = ModalProps & {
 }
 
 export const ExportModal: FC<ExportModalProps> = memo(
-  ({ type: typeProp, onClose, colors, ...rest }) => {
+  ({ isOpen, type: typeProp, onClose, colors, ...rest }) => {
+    const { t } = useI18n()
     const [type, setType] = useState(typeProp)
+    const { format } = useApp()
+    const { value, loading } = useAsync(async () => {
+      if (!isOpen) return ""
+
+      const res = await fetch("/api/palettes/export", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ type, format, colors }),
+      })
+      const data = (await res.json()) as string
+
+      return data
+    }, [isOpen, type, format, colors])
+    const { onCopy, hasCopied } = useClipboard(value, 5000)
 
     useUpdateEffect(() => {
       setType(typeProp)
     }, [typeProp])
 
     return (
-      <Modal onClose={onClose} {...rest}>
+      <Modal isOpen={isOpen} onClose={onClose} {...rest}>
         <ModalCloseButton rounded="full" colorScheme="neutral" />
 
-        <ModalHeader gap="sm">
-          <ExportModalTitle type={type} />
-        </ModalHeader>
+        <ModalHeader gap="sm">{t("component.export-modal.title")}</ModalHeader>
 
-        <ModalBody>
-          <Select
-            value={type}
-            onChange={(value) => setType(value as ColorExport)}
-            items={items}
-          />
+        <ModalBody overflow="visible">
+          <HStack w="full" gap="sm">
+            <Select
+              value={type}
+              onChange={(value) => setType(value as ColorExport)}
+              items={items}
+            />
 
-          <ExportModalData type={type} colors={colors} />
+            <IconButton
+              bg={hasCopied ? "success" : ["blackAlpha.800", "whiteAlpha.800"]}
+              colorScheme="neutral"
+              icon={hasCopied ? <Check /> : <Clipboard />}
+              pointerEvents={hasCopied ? "none" : "auto"}
+              color={["whiteAlpha.900", "blackAlpha.900"]}
+              _hover={{
+                bg: hasCopied
+                  ? "success"
+                  : ["blackAlpha.900", "whiteAlpha.900"],
+              }}
+              borderColor="transparent"
+              onClick={onCopy}
+            />
+          </HStack>
+
+          <ScrollArea
+            type="scroll"
+            w="full"
+            h={{ base: "4xl" }}
+            p="md"
+            whiteSpace="pre"
+            rounded="md"
+            bg={["blackAlpha.50", "whiteAlpha.100"]}
+            color="muted"
+            position="relative"
+          >
+            {!loading ? value : "Loading…"}
+          </ScrollArea>
         </ModalBody>
       </Modal>
     )
@@ -84,60 +133,3 @@ export const ExportModal: FC<ExportModalProps> = memo(
 )
 
 ExportModal.displayName = "ExportModal"
-
-type ExportModalTitleProps = { type: ColorExport }
-
-const ExportModalTitle: FC<ExportModalTitleProps> = ({ type }) => {
-  const [extension, isTones] = type.split(".")
-
-  return (
-    <>
-      <Text as="h1" textTransform="uppercase">
-        {extension}
-      </Text>
-
-      {isTones ? (
-        <Tag size="sm" variant="muted">
-          Tones
-        </Tag>
-      ) : null}
-    </>
-  )
-}
-
-type ExportModalDataProps = {
-  type: ColorExport
-  colors: Colors | ReorderColors
-}
-
-const ExportModalData: FC<ExportModalDataProps> = ({ type, colors }) => {
-  const { format } = useApp()
-  const { value, loading } = useAsync(async () => {
-    const res = await fetch("/api/palettes/export", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ type, format, colors }),
-    })
-    const data = (await res.json()) as string
-
-    return data
-  }, [type, format, colors])
-
-  return (
-    <ScrollArea
-      type="scroll"
-      w="full"
-      h={{ base: "4xl" }}
-      p="md"
-      whiteSpace="pre"
-      rounded="md"
-      bg={["blackAlpha.50", "whiteAlpha.100"]}
-      color="muted"
-      position="relative"
-    >
-      {!loading ? value : "Loading…"}
-    </ScrollArea>
-  )
-}

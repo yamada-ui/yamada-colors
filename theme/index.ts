0808148004660172b9c5196101478154a09d249b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-09 00:59:52 +0900

    refactor(theme): removed unused colorSchemes logic

--- theme/index.ts
+++ theme/index.ts
@@ -1,4 +1,4 @@
-import type { UsageTheme, ThemeSchemes } from "@yamada-ui/react"
+import type { UsageTheme } from "@yamada-ui/react"
 import { extendConfig, extendTheme } from "@yamada-ui/react"
 import { components } from "./components"
 import { customConfig } from "./config"
@@ -6,46 +6,10 @@ import { semantics } from "./semantics"
 import { globalStyle } from "./styles"
 import { tokens } from "./tokens"
 
-const colorSchemes = [
-  "gray",
-  "neutral",
-  "red",
-  "rose",
-  "pink",
-  "flashy",
-  "fuchsia",
-  "purple",
-  "violet",
-  "indigo",
-  "blue",
-  "sky",
-  "cyan",
-  "teal",
-  "emerald",
-  "green",
-  "lime",
-  "yellow",
-  "amber",
-  "orange",
-]
-
 export const customTheme: UsageTheme = {
   styles: { globalStyle },
   semantics,
   components,
-  themeSchemes: colorSchemes.reduce(
-    (prev, colorScheme) => ({
-      ...prev,
-      [colorScheme]: {
-        semantics: {
-          colors: { primary: `${colorScheme}.500` },
-          colorSchemes: { primary: colorScheme },
-        },
-      },
-    }),
-    {} as ThemeSchemes,
-  ),
-  colorSchemes,
   ...tokens,
 }
 

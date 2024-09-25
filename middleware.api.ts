e18b71ca9e3f00e007c16859b115b4789e1e232e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-09-26 00:28:07 +0900

    feat(middleware): added ip extraction and logging

--- middleware.api.ts
+++ middleware.api.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export const middleware = ({ headers }: NextRequest) => {
+export const middleware = ({ headers, ip }: NextRequest) => {
   const blockedUserAgents = process.env.BLOCKED_USER_AGENTS?.split(",") ?? []
 
   const userAgent = headers.get("user-agent")
+  ip ??=
+    headers.get("x-real-ip") ?? headers.get("x-forwarded-for")?.split(",").at(0)
+
+  console.log(ip)
 
   if (userAgent) {
     const isBlockedUserAgent = (blockedUserAgent: string) =>

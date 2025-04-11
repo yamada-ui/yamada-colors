ad2ffa83377cb1b95714a0dfede5264ffe7d9ddc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-04-12 03:12:18 +0900

    refactor(middleware.api): removed IP and user agent blocking logic

--- middleware.api.ts
+++ middleware.api.ts
@@ -6,35 +6,11 @@ export const config = {
 }
 
 export const middleware = ({ headers }: NextRequest) => {
-  const blockedUserAgents = process.env.BLOCKED_USER_AGENTS?.split(",") ?? []
-  const blockedIpAddresses = process.env.BLOCKED_IP_ADDRESSES?.split(",") ?? []
-
-  const userAgent = headers.get("user-agent")
   const xRealIp = headers.get("x-real-ip")
   const xForwardedFor = headers.get("x-forwarded-for")
   const ip = xRealIp ?? xForwardedFor?.split(",").at(0)
 
-  if (userAgent) {
-    const isBlockedUserAgent = (blockedUserAgent: string) =>
-      userAgent
-        .toLocaleUpperCase()
-        .includes(blockedUserAgent.toLocaleUpperCase())
-
-    if (blockedUserAgents.some(isBlockedUserAgent)) {
-      return new NextResponse("Access Denied", { status: 403 })
-    }
-  }
-
-  if (ip) {
-    const isBlockedIpAddress = (blockedIpAddress: string) =>
-      ip.includes(blockedIpAddress.toLocaleUpperCase())
-
-    if (blockedIpAddresses.some(isBlockedIpAddress)) {
-      return new NextResponse("Access Denied", { status: 403 })
-    } else {
-      console.log(ip)
-    }
-  }
+  console.log(ip)
 
   return NextResponse.next()
 }
